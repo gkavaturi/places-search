fab4250d3299916904815e3b14820b8b666bd35a
gokul.kavaturi <goku.post@gmail.com>
2013-04-14 02:04:58 -0700

    added map options

--- js/maps.js
+++ js/maps.js
@@ -1,23 +1,52 @@
 (function(){
 	FoodTruck.maps.init = init = function (){
 		var map,
-			myLatlng = new google.maps.LatLng(-25.363882,131.044922); 	
+			currentLocation = new google.maps.LatLng(37.34, -121.89694); 	
 			mapOptions = {
-	          zoom: 8,
-	          center: new google.maps.LatLng(-34.397, 150.644),
-	          mapTypeId: google.maps.MapTypeId.ROADMAP
+	          zoom: 15,
+	          center: currentLocation,
+	          mapTypeId: google.maps.MapTypeId.HYBRID
 	        };
-	    map = new google.maps.Map(document.getElementById('map-canvas'),
-	            mapOptions);
+	    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+	    FoodTruck.location = currentLocation;
+	    FoodTruck.map = map;
+
 	}
 
-	FoodTruck.maps.setMarker = setMarker = function() {
-		if (!Maps.map) {
-			console.log('map is not initialized')
-		}
-		
+	FoodTruck.maps.setMarker = setMarker = function(query) {
+		if (!query){
+      console.log('No query specified');
+      return;
+    }
+    var currentLocation = FoodTruck.location,
+			map = FoodTruck.map, 	
+			request = {
+				location: currentLocation,
+				radius: '200',
+				query: query  	
+			};
+		service = new google.maps.places.PlacesService(map);
+		service.textSearch(request, function(data) {
+			if (!(data instanceof Array)){
+				console.log('ERROR: invalid response from server');
+				return;
+			}
+      console.log(data);
+			data.forEach(function(obj){
+				 var marker = new google.maps.Marker({
+		            position: obj.geometry.location,
+		            map: map,
+		            title: obj.name
+		        });
+			});
+		});
 	}
 
 	init();
+
+  ['schools',  'parking'].forEach(function(query){
+    setMarker(query);
+  });
 
 }());
